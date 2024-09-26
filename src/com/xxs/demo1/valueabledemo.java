dcc285c2e31017c407a1a7842d4c05375c7c9aaf
xiaoxiaosui <1057379513@qq.com>
2024-09-26 19:30:15 +0800

    git remote add origin https://github.com/xiaoxsui/basic-code.git git branch -M main git push -u origin main

---
+++ src/com/xxs/demo1/valueabledemo.java
@@ -0,0 +1,19 @@
+package com.xxs.demo1;
+
+public class valueabledemo {
+    public static void main(String[] args) {
+        //打印整数
+        System.out.println(123);
+        //打印小数
+        System.out.println(1.23);
+        //打印字符串
+        System.out.println("helloworld");
+        //打印字符
+        System.out.println('1');
+        //打印布尔值
+        System.out.println(true);
+        System.out.println(false);
+        //打印空值
+        System.out.println("null");
+    }
+}
---
+++ src/com/xxs/demo1/variabledemo.java
@@ -0,0 +1,36 @@
+package com.xxs.demo1;
+// 学习8种不同的数据类型
+public class variabledemo {
+    public static void main(String[] args) {
+        //byte
+        byte b = 10;
+        System.out.println(b);
+        //short
+        short s = 20;
+        System.out.println(s);
+        //int
+        int i = 30;
+        System.out.println(i);
+        //long
+        //注:定义long类型变量时,需在数据值的后面添加一个L作为后缀
+        long n = 1234567890L;
+        System.out.println(n);
+
+        //float
+        //注:需使用F作为后缀
+        float f = 10.1F;
+        System.out.println(f);
+        //double
+        double d = 20.2;
+        System.out.println(d);
+
+        //char
+        char c = '中';
+        System.out.println(c);
+
+        //boolean
+        boolean o = true;
+        System.out.println(o);
+
+    }
+}
